e348a8f03a3cf39ecbe98247ff9bd2e29f290040
Diego EM <dmunozaguilar2000@gmail.com>
2022-02-16 14:47:06 -0700

    test deleted

--- src/app/directives/note-container.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoteContainerDirective } from './note-container.directive';
-
-describe('NoteContainerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NoteContainerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
